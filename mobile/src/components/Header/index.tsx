6bab91782b4921ce36997c12c82d3499c7f3e105
Bruno Henrique <bruno@cbkdigital.com.br>
2021-08-20 00:27:21 -0400

    chore(mobile): implemented header component

--- mobile/src/components/Header/index.tsx
+++ mobile/src/components/Header/index.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import { Container, Title, Icon, RightIcons } from './styles';
+
+type IIConButtonProps = {
+  name: keyof typeof Icon.glyphMap;
+  onPress(): void;
+};
 
 type IHeaderProps = {
   title?: string;
-  // leftIcon:
+  leftIcon?: IIConButtonProps;
+  rightIcons?: IIConButtonProps[];
 };
 
-const Header: React.FC<IHeaderProps> = ({ title }) => {
+const Header: React.FC<IHeaderProps> = ({ title, leftIcon, rightIcons }) => {
   return (
     <Container>
+      {leftIcon && <Icon {...leftIcon} />}
+
       <Title>{title}</Title>
+
+      <RightIcons>
+        {rightIcons?.map(iconButton => (
+          <Icon key={iconButton.name} {...iconButton} />
+        ))}
+      </RightIcons>
     </Container>
   );
 };
--- mobile/src/components/Header/styles.ts
+++ mobile/src/components/Header/styles.ts
@@ -1,13 +1,27 @@
 import styled from 'styled-components/native';
 import { StatusBar } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 export const Container = styled.View`
+  flex-direction: row;
+  align-items: center;
   margin-top: ${StatusBar.currentHeight || 30}px;
   padding: 15px 0;
 `;
 
 export const Title = styled.Text`
+  flex: 1;
   color: #fff;
   font-size: 20px;
   font-weight: bold;
+`;
+
+export const Icon = styled(Feather)`
+  color: #fff;
+  font-size: 24px;
+  margin-right: 10px;
+`;
+
+export const RightIcons = styled.View`
+  flex-direction: row;
 `;
